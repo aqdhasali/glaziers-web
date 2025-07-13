f3555a9bbd6a9c5e5e02ed0c25b794ee6dbe6453
aqdhasali <aqdhas@commercializer.global>
2025-07-13 14:53:53 +0530

    bug fixes

--- src/components/Home/HeroContainer.jsx
+++ src/components/Home/HeroContainer.jsx
@@ -40,7 +40,7 @@ export default function HeroContainer() {
         playsInline
         className="absolute top-0 left-0 w-full h-full object-cover z-0"
       >
-        <source src="/src/assets/videos/hero-video.mp4" type="video/mp4" />
+        <source src="/public/videos/hero-video.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
